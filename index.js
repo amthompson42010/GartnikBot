ba7223c4f1643e8d11413fecdfd70e56a1e5ddd1
Alex Thompson <amthompson42010@gmail.com>
2021-07-15 01:44:25 -0500

    Add a bunch of commands for socials. Also changed the if structure to a switch statement.

--- index.js
+++ index.js
@@ -14,10 +14,31 @@ const client = new tmi.Client({
 
 client.connect();
 
+
 client.on('message', (channel, tags, message, self) => {
     if(self) return;
 
-    if(message.toLowerCase() === 'hello') {
-        client.say(channel, `@${tags.username}, welcome to the stream!`);
+    console.log(message.toLowerCase())
+    switch(message.toLowerCase()) {
+        case 'hello':
+            client.say(channel, `Welcome to the stream @${tags.username}!`);
+            break;
+        case '!twitter':
+            client.say(channel, `https://twitter.com/GartnikF`);
+            break;
+        case '!youtube':
+            client.say(channel, `https://www.youtube.com/channel/UCCAkMS2U32q9DrYthAR5rjw`);
+            break;
+        case '!discord':
+            client.say(channel, `https://discord.com/invite/mjdBMN`);
+            break;
+        case '!insta':
+            client.say(channel, `https://www.instagram.com/gartnikflamestwitch/?hl=en`);
+            break;
+        case '!github':
+            client.say(channel, `https://github.com/amthompson42010`);
+            break;
+        default:      
     }
+
 });
